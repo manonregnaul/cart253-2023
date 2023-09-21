53833ecad5f563970bfb66e91afcde94ca41758e
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-21 10:19:51 -0400

    A3: Completed the activity

--- Activity/03-moving pictures/js/script.js
+++ Activity/03-moving pictures/js/script.js
@@ -1,19 +1,43 @@
 /**
- * Title of Project
- * Author Name
+ 03 - Moving pictures activity
+ Manon Regnault
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+This is an attempt to reproduce the moving pictures activity from Pippin Bar video tutorial.
  */
 
 "use strict";
 
+
 /**
- * Description of preload
+ * Created a canvas
 */
 function preload() {
-
-}
+    let bg = {
+        r: 0,
+        g: 0,
+        b: 0
+    };
+    
+    let circle1 = {
+        x: 0,
+        y: 250,
+        size: 100,
+        growthRate: 1, 
+        speed: 1, 
+        fill: 255, 
+        alpha: 200
+    };
+    
+    let circle2= {
+        x: 500,
+        y: 250, 
+        size: 75, 
+        sizeRatio: 0.75, 
+        speed: -1,
+        fill: 255, 
+        alpha: 200
+    };
+    
 
 
 /**
@@ -21,12 +45,33 @@ function preload() {
 */
 function setup() {
 
+    createCanvas(500, 500);
+    noStroke();
+}
 }
 
 
 /**
- * Description of draw()
+ * Create a bg that changes color over time (red)
 */
 function draw() {
+    // Background
+    background(bg.r,bg.g,bg.b); 
+    bg.r = map(circle1.size, 100, width, 0, 255);
+
+    // Left circle
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width/2);
+    circle1.size = circle1.size + circle1.growthRate; 
+    circle1.size = constrain(circle1.size, 0, width);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    // Right circle
+    circle2.x = circle2.x + circle2.speed; 
+    circle2.x = constrain(circle2.x, width/2, width)
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
 
 }
