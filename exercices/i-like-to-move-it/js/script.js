2c48abe7a35fe5c63cccaebb8ebb62ea524932c0
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-21 15:18:45 -0400

    E1: Started the exercice and experimentation

--- exercices/i-like-to-move-it/js/script.js
+++ exercices/i-like-to-move-it/js/script.js
@@ -1,17 +1,18 @@
 /**
- * Title of Project
- * Author Name
+ * E1: I like to move it
+ * Manon Regnault
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Create a program full of shapes that move, change size and color, and anything else you want to experiment with.
  */
 
 "use strict";
 
+    
 /**
  * Description of preload
 */
 function preload() {
+    
 
 }
 
@@ -19,8 +20,24 @@ function preload() {
 /**
  * Description of setup
 */
-function setup() {
 
+ // Declare the variables 
+ let backgroundShade = 90;
+
+ let circle = {
+    x: 0,
+    y: 250, 
+    size: 100, 
+    speed: 1,
+ };
+
+
+
+
+function setup() {
+    createCanvas(500, 500);
+    background(7, 224, 174, 88);
+    
 }
 
 
@@ -28,5 +45,9 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-
+    //background(backgroundShade);
+    //noStroke();
+    ellipse(circle.x, circle.y, circle.size);
+    circle.x = circle.x + circle.speed; 
+    
 }
