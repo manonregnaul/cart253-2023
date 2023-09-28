39116b8a50b3919facb5ba084d83c370f6a5ff68
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-28 08:32:49 -0400

    E3: More conditionals and using else

--- experiments/conditionales/js/script.js
+++ experiments/conditionales/js/script.js
@@ -12,7 +12,7 @@ let circle = {
     x: 0, 
     y: 250,
     size: 100,
-    speed: 5
+    speed: 1
 }
 
 
@@ -37,21 +37,19 @@ function draw() {
 
     circle.x = circle.x + circle.speed;
 
-    if (circle.x > width) {
-        circle.speed = -circle.speed;
-    }
-
-    if (circle.x < 0) {
-        circle.speed = -circle.speed;
-    }
-
-    if (mouseY < height/2) {
+    if (mouseX < width/3) {
         fill(255, 0, 0);
     }
-
-    if (mouseY >  height/2) {
+    else if (mouseX < 2 * width/3) {
+        fill(0, 255, 0);
+    }
+    else {
         fill(0, 0, 255);
     }
+        
+    }
+     
+
     ellipse(circle.x, circle.y, circle.size);
     
 
