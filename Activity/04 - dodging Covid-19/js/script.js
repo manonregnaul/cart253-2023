daa4f1df968e89f0d3ced2cbcb1742b448d919e5
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-28 17:18:44 -0400

    A3: Finished the activity

--- Activity/04 - dodging Covid-19/js/script.js
+++ Activity/04 - dodging Covid-19/js/script.js
@@ -22,6 +22,17 @@ let covid19 = {
     }
 }
 
+let user = {
+    x: 250,
+    y: 250, 
+    size: 100, 
+    fill: 255
+    }
+
+
+let numStatic = 500;
+
+
 
 function preload() {
 
@@ -36,6 +47,8 @@ function setup() {
 
      covid19.y = random(0, height); 
      covid19.vx = covid19.speed; 
+
+     noCursor();
 }
 
 
@@ -45,12 +58,50 @@ function setup() {
 function draw() {
     background(0);
 
+    // Display static
+    for (let i = 0; i < numStatic; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        stroke(255);
+        point(x,y);
+    }
+
+
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(255);
+    point(x,y);
+
+
+
+    // Covid 19 movement 
     covid19.x = covid19.x + covid19.vx;
     covid19.y = covid19.y + covid19.vy;
 
-    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-    ellipse(covid19.x, covid19.y, covid19.size)
+    if (covid19 > width) {
+        covid19.x = 0;
+        covid19.y = random(0, height)
+    }
 
+    // User movement 
+    user.x = mouseX
+    user.y = mouseY
+
+    // Check for catching covid19
+    let d = dist(user.x, user.y, covid19.x, covid19.y)
+    if (d < covid19.size/2 + user.size/2) {
+        noLoop();
+
+    }
+
+    // Display covid 19
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x, covid19.y, covid19.size);
+
+
+    // Display user
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
 
 
 }
