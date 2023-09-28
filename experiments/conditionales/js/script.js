c49febe6da1d17e4f4c8b44d2be9559da188cf94
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-28 12:10:22 -0400

    E3: Created velocity - conditional experiments

--- experiments/conditionales/js/script.js
+++ experiments/conditionales/js/script.js
@@ -7,10 +7,21 @@
 
 "use strict";
 
+
+let bg = {
+    r: 255,
+    b: 0,
+    g: 0
+}
+
 let circle = {
     x: 250, 
     y: 250, 
-    size: 100
+    size: 100, 
+    vx: 0,
+    vy: 0,
+    speed: 5 
+
 };
 
 
@@ -35,15 +46,28 @@ function setup() {
  * Drawing a black bg and a moving circle
 */
 function draw() {
-    background (0);
+    background (bg.r, bg.g, bg.b);
+
+    if (mouseX < circle.x) {
+        circle.vx = -circle.speed;
+    }
+    else {
+        circle.vx = circle.speed; 
+    }
+
+    if (mouseY < circle.y) {
+        circle.vy = -circle.speed;
+    }
+    else{
+        circle.vy = circle.speed;
+    }
+
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
 
     ellipse(circle.x, circle.y, circle.size);
 }
 
-function mousePressed () {
-     circle.x = mouseX;
-     circle.y = mouseY;
-}
 
   
     
