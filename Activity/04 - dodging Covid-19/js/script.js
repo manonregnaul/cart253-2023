9077b2a4e35e1eb84a3ace2fce61f092d3b3fdd3
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-28 16:53:19 -0400

    A3: Created the setup and drawing main elements that we'll use

--- Activity/04 - dodging Covid-19/js/script.js
+++ Activity/04 - dodging Covid-19/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Activity Dodging Covid-19
+ * Manon Regnault
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -8,19 +8,34 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+let covid19 = {
+    x: 0,
+    y: 250, 
+    size: 100, 
+    vx: 0, 
+    vy: 0, 
+    speed: 5, 
+    fill: {
+        r: 255, 
+        g: 0, 
+        b: 0
+    }
+}
+
+
 function preload() {
 
 }
 
 
 /**
- * Description of setup
+ *  Created a canvas et our covid19 variable somewhere randomly 
 */
 function setup() {
+     createCanvas(windowWidth, windowHeight);
 
+     covid19.y = random(0, height); 
+     covid19.vx = covid19.speed; 
 }
 
 
@@ -28,5 +43,14 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x, covid19.y, covid19.size)
+
+
 
 }
