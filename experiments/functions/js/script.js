bb432cc1a7eaf9a46a1b1ad7353e5511aad8bd07
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-30 11:03:53 -0400

    E3: Changed de size, speed of Hello World

--- experiments/functions/js/script.js
+++ experiments/functions/js/script.js
@@ -6,7 +6,14 @@
 
 "use strict";
 
-
+let hello = {
+    string: 'Hello, world!', 
+    x: 250, 
+    y: 250, 
+    vx: 5, 
+    vy: 1, 
+    size: 64
+}
 
 function setup() {
     createCanvas(500, 500);
@@ -19,12 +26,21 @@ function setup() {
 function draw() {
     background(0);
 
+    hello.x = hello.x + hello.vx;
+    hello.y = hello.y + hello.vy;
+
+
+    hello.size += 1;
+
 
     textAlign(CENTER, CENTER);
     textSize(64);
+    textStyle(BOLD);
 
 
     fill(200, 50, 200);
+    stroke(50, 200, 50);
+    strokeWeight(3);
 
 
     text('Hello, world!', 250, 250);
