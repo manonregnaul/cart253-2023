27d5b81de34bc5c92d9453c421f7b56f63400d31
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-09-30 10:52:47 -0400

    E3: Displayed text

--- experiments/functions/js/script.js
+++ experiments/functions/js/script.js
@@ -11,11 +11,6 @@
 function setup() {
     createCanvas(500, 500);
 
-    let hotCelsisus = toCelsius(100);
-    console.log('100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.');
-
-    let coldCelsius = toCelsius(10);
-    console.log('10 degrees Fahrenheit is ${coldCelsius} degrees Celsius');
 
 }
 
@@ -23,10 +18,9 @@ function setup() {
 
 function draw() {
     background(0);
+    rect(250, 250, 10, 10);
+    text('Hello, world!', 250, 250);
+
 
 }
 
-function toCelsius(fahrenheit) {
-    let celsius = (fahrenheit - 32) * 5/9;
-    return celsius;
-}
