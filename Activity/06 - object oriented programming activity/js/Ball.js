652556fb6c9542fdc948d6afb46d11bcc4dab234
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-10-27 13:33:55 -0400

    A5: Added some properties and methods the the Ball class

--- Activity/06 - object oriented programming activity/js/Ball.js
+++ Activity/06 - object oriented programming activity/js/Ball.js
@@ -12,4 +12,35 @@ class Ball {
         this.active = true;
 
     }
+
+    gravity(force){
+        this.ay += force; 
+    }
+
+    move(){
+        this.vx += this.ax;
+        this.vy += this.ay;
+
+        this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+        this.vy = constrain(this.vy, - this.maxSpeed, this.maxSpeed);
+
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    bounce(){
+        if (this.y + this.size/2 >= height){
+            this.vy = - this.vy;
+            this.ay = 0;
+        }
+    }
+
+    display(){
+        push();
+        fill(255, 50, 50);
+        stroke(0);
+        ellipse(this.x, this.y, this.size);
+        pop();
+    }
+
 }
