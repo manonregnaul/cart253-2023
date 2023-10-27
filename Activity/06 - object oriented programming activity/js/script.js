5d1b21be221db9a92de57c1036c8c4d42d30a297
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-10-27 13:01:23 -0400

    A5: Created new class files and put them in the indez.html

--- Activity/06 - object oriented programming activity/js/script.js
+++ Activity/06 - object oriented programming activity/js/script.js
@@ -2,8 +2,10 @@
  * Object Oriented Programming activity 
  * Manon Regnault
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * The user will control a rectangular "paddle" at the bottom of the
+ * screen, moving it horizontally with the mouse position. Balls will fall from
+ * the top of the screen. If they hit the paddle, they will bounce upward
+ * and fall again. If they miss the paddle, they will disappear off the bottom.
  */
 
 "use strict";
