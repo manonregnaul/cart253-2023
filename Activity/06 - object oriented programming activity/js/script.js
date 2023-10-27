59ba7d5c6d0db4b1be2b2eb5fb304f8ffe9c03c2
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-10-27 13:43:39 -0400

    A5: use the ball objects by calling their methods

--- Activity/06 - object oriented programming activity/js/script.js
+++ Activity/06 - object oriented programming activity/js/script.js
@@ -10,14 +10,26 @@
 
 "use strict";
 
+
+let gravityForce = 0.0025;
+
 let paddle;
 
+let balls = [];
+let numBalls = 3
 
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
     paddle = new Paddle(300, 20);
+
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, - 100);
+        let ball = new Ball(x, y);
+        balls.push(ball);
+    }
 
 }
 
@@ -29,4 +41,11 @@ function draw() {
     paddle.move();
     paddle.display();
 
+    for (let i = 0; i < balls.length; i++) { 
+        let ball = balls[i];
+        ball.gravity(gravityForce);
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
 }
