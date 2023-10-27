0fb1c4ff14b3f1963ef7b2e59d4289349b3096ae
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-10-27 13:23:13 -0400

    A5: Added properties to my ball

---
+++ Activity/06 - object oriented programming activity/js/Ball.js
@@ -0,0 +1,15 @@
+class Ball {
+
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.size = 40;
+        this.vx = 0;
+        this.vy = 0;
+        this.ax = 0;
+        this.ay = 0;
+        this.maxSpeed = 10;
+        this.active = true;
+
+    }
+}
