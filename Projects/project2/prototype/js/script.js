756e249ee2fe3ae6a44f77dafc556e22bdd977b6
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-11-07 10:18:22 -0500

    P2: Preloaded my images for my firstPart + added the two different first states

--- Projects/project2/prototype/js/script.js
+++ Projects/project2/prototype/js/script.js
@@ -7,13 +7,37 @@
 
 "use strict";
 
+let state = 'title' // Can also be firstPart, secondPart, thirdPart
+
 // Created variable for my first state of my program 
 let moiImage;
 
 // Created variables for my second state of my program
+let bereniceSourireImage;
+let julietteImage;
+let chicagoImage;
+let troisphotosImage;
+let museeImage;
+let metroImage;
+let maximeAlizeeImage;
+let veloAlizeeImage;
+
 
 function preload() {
+
+// Load the image of my title state
     moiImage = loadImage("assets/images/moi.png");
+
+// Load the images of my first part
+    bereniceSourireImage = loadImage("assets/images/bereniceSourire.png");
+    julietteImage = loadImage("assets/images/juliette.png");
+    chicagoImage = loadImage("assets/images/chicago.png");
+    troisphotosImage = loadImage("assets/images/troisphotos.png");
+    museeImage = loadImage("assets/images/musee.png");
+    metroImage = loadImage("assets/images/metro.png");
+    maximeAlizeeImage = loadImage("assets/images/maximeAlizee.png");
+    veloAlizeeImage = loadImage("assets/images/veloAlizee.png");
+
 }
 
 
@@ -27,9 +51,49 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+
+    // Called our state in a specific order
+    if(state === 'title') {
+        title();
+    }
+    else if (state === 'firstPart'){
+        firstPart();
+    }
+   
+}
+
+function title() {
+    
+
     background(238, 130, 238);
 
-    image(moiImage, windowWidth/1.3, 60, 400, 1200);
+    push();
+    textSize(70);
+    noStroke();
+    fill(116, 31, 56);
+    textFont('BN BOOP');
+    textAlign(CENTER, CENTER);
+    text('welcome to my universe', width/2.4, height/2);
+    pop();
 
-    
+    image(moiImage, windowWidth/1.3, 60, 400, 1200);
+}
+
+function firstPart() {
+    background(116, 31, 56);
+
+
+}
+
+
+
+
+
+function mousePressed() {
+    if (mouseX, mouseY = width/2.4, height/2) {
+        state = 'firstPart'
+    }
+  //  else if (mouseX, mouseY) {
+   //     state = 'secondPart'
+    //}
 }
