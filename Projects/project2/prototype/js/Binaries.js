7bcc6a6c2fb66b32d885cde3f48e76a82a77761a
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-11-09 10:10:41 -0500

    P2: Added methods in my new binaries class in order to display and move them into my program

---
+++ Projects/project2/prototype/js/Binaries.js
@@ -0,0 +1,24 @@
+class Binaries {
+
+    constructor(x, y, r, img) {
+        this.x = x;
+        this.y = y;
+        this. r = r;
+        this.binary = random(binaries);
+    }
+
+    clicked (px, py) {
+        if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
+            this.binary = random(binaries);
+        }
+    }
+
+    move() {
+        this.y = this.y + 3;
+    }
+
+    display() {
+        image(this.binary, this.x, this.y, this.r, this.r);
+    
+    }
+}
