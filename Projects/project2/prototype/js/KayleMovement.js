eac6e03496ba259cab8c12b1b2ab1aac028f2351
manonregnaul <144383310+manonregnaul@users.noreply.github.com>
2023-11-28 10:08:27 -0500

    P2: Created a movement

--- Projects/project2/prototype/js/KayleMovement.js
+++ Projects/project2/prototype/js/KayleMovement.js
@@ -1,16 +1,19 @@
 class KayleMouvement {
 
+  // Acceleration for a natural movement with an initial speed 
   constructor() {
-    this.x = width - 50; // Démarre à droite du canvas
+    this.x = width - 50; 
     this.y = height / 2;
-    this.vx = -1; // Vitesse initiale
+    this.vx = -1; 
     this.vy = 0;
-    this.acceleration = 0.01; // Accélération pour un mouvement naturel
+    this.acceleration = 0.01; 
 }
 
+// Constant acceleration to the left 
 move() {
-
-        
+  this.vx += this.acceleration; 
+  this.x += this.vx;
+}
         
 
 }
